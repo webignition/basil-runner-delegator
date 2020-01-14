21be9c9ed8b66e504eb12c828b02eb3c38fa2c14
Jon Cram <jon@webignition.net>
2020-01-14 12:10:11 +0000

    Model validation result around only failures

--- src/Model/ValidationResult/Command/GenerateCommandValidationResult.php
+++ src/Model/ValidationResult/Command/GenerateCommandValidationResult.php
@@ -9,24 +9,17 @@ use webignition\BasilRunner\Model\GenerateCommandConfiguration;
 class GenerateCommandValidationResult
 {
     private $configuration;
-    private $isValid;
     private $errorCode;
 
-    public function __construct(GenerateCommandConfiguration $configuration, bool $isValid, int $errorCode = 0)
+    public function __construct(GenerateCommandConfiguration $configuration, int $errorCode)
     {
         $this->configuration = $configuration;
-        $this->isValid = $isValid;
         $this->errorCode = $errorCode;
     }
 
     public function getConfiguration(): GenerateCommandConfiguration
     {
         return $this->configuration;
-    }
-
-    public function getIsValid(): bool
-    {
-        return $this->isValid;
     }
 
     public function getErrorCode(): int
