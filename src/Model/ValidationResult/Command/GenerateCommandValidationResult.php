011a92393146b8424b29797e22704fc280dbbe75
Jon Cram <jon@webignition.net>
2020-01-14 09:02:12 +0000

    Use configuration in validator

--- src/Model/ValidationResult/Command/GenerateCommandValidationResult.php
+++ src/Model/ValidationResult/Command/GenerateCommandValidationResult.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace webignition\BasilRunner\Model\ValidationResult\Command;
 
+use webignition\BasilRunner\Model\GenerateCommandConfiguration;
+
 class GenerateCommandValidationResult
 {
+    private $configuration;
     private $isValid;
     private $errorCode;
 
-    public function __construct(bool $isValid, int $errorCode = 0)
+    public function __construct(GenerateCommandConfiguration $configuration, bool $isValid, int $errorCode = 0)
     {
+        $this->configuration = $configuration;
         $this->isValid = $isValid;
         $this->errorCode = $errorCode;
+    }
+
+    public function getConfiguration(): GenerateCommandConfiguration
+    {
+        return $this->configuration;
     }
 
     public function getIsValid(): bool
