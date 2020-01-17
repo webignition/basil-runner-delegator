f1ac38a368e4e7f36a2b480d7a157d276b49ae8a
Jon Cram <jon@webignition.net>
2020-01-17 10:56:49 +0000

    Handle UnknownItemException

--- tests/Functional/Command/GenerateCommandTest.php
+++ tests/Functional/Command/GenerateCommandTest.php
@@ -220,6 +220,7 @@ class GenerateCommandTest extends AbstractFunctionalTest
      * @dataProvider runNonRetrievableImportDataProvider
      * @dataProvider runParseExceptionDataProvider
      * @dataProvider runUnknownElementDataProvider
+     * @dataProvider runUnknownItemDataProvider
      */
     public function testRunFailure(
         array $input,
@@ -877,6 +878,108 @@ class GenerateCommandTest extends AbstractFunctionalTest
                             '/tests/Fixtures/basil/InvalidTest/import-action-containing-unknown-element.yml',
                         'step_name' => 'use action_contains_unknown_element',
                         'statement' => 'click $elements.unknown_element_name',
+                    ]
+                ),
+            ],
+        ];
+    }
+
+    public function runUnknownItemDataProvider(): array
+    {
+        $root = (new ProjectRootPathProvider())->get();
+
+        return [
+            'test declares step, step uses unknown dataset' => [
+                'input' => [
+                    '--source' => 'tests/Fixtures/basil/InvalidTest/step-uses-unknown-dataset.yml',
+                    '--target' => 'tests/build/target',
+                ],
+                'expectedExitCode' => ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                'expectedCommandOutput' => new ErrorOutput(
+                    new Configuration(
+                        $root . '/tests/Fixtures/basil/InvalidTest/step-uses-unknown-dataset.yml',
+                        $root . '/tests/build/target',
+                        AbstractBaseTest::class
+                    ),
+                    'Unknown dataset "unknown_data_provider_name"',
+                    ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                    [
+                        'type' => 'dataset',
+                        'name' => 'unknown_data_provider_name',
+                        'test_path' => $root . '/tests/Fixtures/basil/InvalidTest/step-uses-unknown-dataset.yml',
+                        'step_name' => 'step name',
+                        'statement' => '',
+                    ]
+                ),
+            ],
+            'test declares step, step uses unknown page' => [
+                'input' => [
+                    '--source' => 'tests/Fixtures/basil/InvalidTest/step-uses-unknown-page.yml',
+                    '--target' => 'tests/build/target',
+                ],
+                'expectedExitCode' => ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                'expectedCommandOutput' => new ErrorOutput(
+                    new Configuration(
+                        $root . '/tests/Fixtures/basil/InvalidTest/step-uses-unknown-page.yml',
+                        $root . '/tests/build/target',
+                        AbstractBaseTest::class
+                    ),
+                    'Unknown page "unknown_page_import"',
+                    ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                    [
+                        'type' => 'page',
+                        'name' => 'unknown_page_import',
+                        'test_path' => $root . '/tests/Fixtures/basil/InvalidTest/step-uses-unknown-page.yml',
+                        'step_name' => 'step name',
+                        'statement' => '',
+                    ]
+                ),
+            ],
+            'test declares step, step uses step' => [
+                'input' => [
+                    '--source' => 'tests/Fixtures/basil/InvalidTest/step-uses-unknown-step.yml',
+                    '--target' => 'tests/build/target',
+                ],
+                'expectedExitCode' => ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                'expectedCommandOutput' => new ErrorOutput(
+                    new Configuration(
+                        $root . '/tests/Fixtures/basil/InvalidTest/step-uses-unknown-step.yml',
+                        $root . '/tests/build/target',
+                        AbstractBaseTest::class
+                    ),
+                    'Unknown step "unknown_step"',
+                    ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                    [
+                        'type' => 'step',
+                        'name' => 'unknown_step',
+                        'test_path' => $root . '/tests/Fixtures/basil/InvalidTest/step-uses-unknown-step.yml',
+                        'step_name' => 'step name',
+                        'statement' => '',
+                    ]
+                ),
+            ],
+            'test suite imports test declaring step, step uses unknown dataset' => [
+                'input' => [
+                    '--source' =>
+                        'tests/Fixtures/basil/InvalidTestSuite/imports-test-declaring-step-using-unknown-dataset.yml',
+                    '--target' => 'tests/build/target',
+                ],
+                'expectedExitCode' => ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                'expectedCommandOutput' => new ErrorOutput(
+                    new Configuration(
+                        $root .
+                        '/tests/Fixtures/basil/InvalidTestSuite/imports-test-declaring-step-using-unknown-dataset.yml',
+                        $root . '/tests/build/target',
+                        AbstractBaseTest::class
+                    ),
+                    'Unknown dataset "unknown_data_provider_name"',
+                    ErrorOutput::CODE_LOADER_UNKNOWN_ITEM,
+                    [
+                        'type' => 'dataset',
+                        'name' => 'unknown_data_provider_name',
+                        'test_path' => $root . '/tests/Fixtures/basil/InvalidTest/step-uses-unknown-dataset.yml',
+                        'step_name' => 'step name',
+                        'statement' => '',
                     ]
                 ),
             ],
