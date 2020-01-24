ead47a57f66468039b124a07e3bc7a3c38058004
Jon Cram <webignition@gmail.com>
2020-01-24 10:16:00 +0000

    Create empty ResultPrinter (#168)

---
+++ src/Services/ResultPrinter.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilRunner\Services;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestListener;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\Warning;
+use PHPUnit\Util\Printer;
+
+class ResultPrinter extends Printer implements TestListener
+{
+    /**
+     * @inheritDoc
+     */
+    public function addError(Test $test, \Throwable $t, float $time): void
+    {
+        // TODO: Implement addError() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addWarning(Test $test, Warning $e, float $time): void
+    {
+        // TODO: Implement addWarning() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addFailure(Test $test, AssertionFailedError $e, float $time): void
+    {
+        // TODO: Implement addFailure() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addIncompleteTest(Test $test, \Throwable $t, float $time): void
+    {
+        // TODO: Implement addIncompleteTest() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addRiskyTest(Test $test, \Throwable $t, float $time): void
+    {
+        // TODO: Implement addRiskyTest() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addSkippedTest(Test $test, \Throwable $t, float $time): void
+    {
+        // TODO: Implement addSkippedTest() method.
+    }
+
+    /**
+     * @param TestSuite<Test> $suite
+     */
+    public function startTestSuite(TestSuite $suite): void
+    {
+        // TODO: Implement startTestSuite() method.
+    }
+
+    /**
+     * @param TestSuite<Test> $suite
+     */
+    public function endTestSuite(TestSuite $suite): void
+    {
+        // TODO: Implement endTestSuite() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function startTest(Test $test): void
+    {
+        // TODO: Implement startTest() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function endTest(Test $test, float $time): void
+    {
+        // TODO: Implement endTest() method.
+    }
+}
