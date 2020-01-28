042ff5461e248bc7893af4600be42efb53e9b2de
Jon Cram <jon@webignition.net>
2020-01-28 09:31:46 +0000

    Add path option to run command

---
+++ tests/Functional/Command/RunCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilRunner\Tests\Functional\Command;
+
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use webignition\BasilRunner\Command\RunCommand;
+use webignition\BasilRunner\Tests\Functional\AbstractFunctionalTest;
+
+class RunCommandTest extends AbstractFunctionalTest
+{
+    /**
+     * @var RunCommand
+     */
+    private $command;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->command = self::$container->get(RunCommand::class);
+    }
+
+    public function testRunFailurePathDoesNotExist()
+    {
+        $input = [
+            '--path' => __DIR__ . '/non-existent',
+        ];
+
+        $output = new BufferedOutput();
+
+        $exitCode = $this->command->run(new ArrayInput($input), $output);
+        $this->assertSame(RunCommand::RETURN_CODE_INVALID_PATH, $exitCode);
+
+        $commandOutputContent = $output->fetch();
+        $this->assertEquals('', $commandOutputContent);
+    }
+}
