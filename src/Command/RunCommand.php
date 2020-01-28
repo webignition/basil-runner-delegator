5a6d7a6e9908d9fe3d6cb3f8e96a01b4057e7c97
Jon Cram <webignition@gmail.com>
2020-01-28 10:28:23 +0000

    Add path option to run command (#180)

--- src/Command/RunCommand.php
+++ src/Command/RunCommand.php
@@ -6,30 +6,68 @@ namespace webignition\BasilRunner\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use webignition\BasilRunner\Services\ProjectRootPathProvider;
+use webignition\SymfonyConsole\TypedInput\TypedInput;
 
 class RunCommand extends Command
 {
+    public const OPTION_PATH = 'path';
+
+    public const RETURN_CODE_INVALID_PATH = 100;
+
     private const NAME = 'basil-runner:run';
+    private const DEFAULT_RELATIVE_PATH = '/generated';
+
+    private $projectRootPath;
+
+    public function __construct(ProjectRootPathProvider $projectRootPathProvider)
+    {
+        $this->projectRootPath = $projectRootPathProvider->get();
+
+        parent::__construct();
+    }
 
     protected function configure(): void
     {
         $this
             ->setName(self::NAME)
             ->setDescription('Command description')
+            ->addOption(
+                self::OPTION_PATH,
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Absolute path to the directory of tests to run.',
+                $this->projectRootPath . self::DEFAULT_RELATIVE_PATH
+            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $typedInput = new TypedInput($input);
+
+        $path = trim((string) $typedInput->getStringOption(RunCommand::OPTION_PATH));
+        if (!is_dir($path)) {
+            return self::RETURN_CODE_INVALID_PATH;
+        }
+
         $execOutput = [];
         $returnCode = 0;
-        $command = __DIR__ . '/../../vendor/bin/phpunit -c ' . __DIR__ . '/../../phpunit.run.xml --colors=always';
 
-        exec($command, $execOutput, $returnCode);
+        exec($this->createPhpUnitCommand($path), $execOutput, $returnCode);
 
         $output->writeln(implode("\n", $execOutput));
 
         return 0;
+    }
+
+    private function createPhpUnitCommand(string $path): string
+    {
+        $phpUnitExecutablePath = $this->projectRootPath . '/vendor/bin/phpunit';
+        $phpUnitConfigurationPath = $this->projectRootPath . '/phpunit.run.xml';
+
+        return $phpUnitExecutablePath . ' -c ' . $phpUnitConfigurationPath . ' --colors=always ' . $path;
     }
 }
