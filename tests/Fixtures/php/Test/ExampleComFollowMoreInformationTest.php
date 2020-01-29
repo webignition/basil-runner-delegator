3fac6d5786e32c281c44aff421d6b038f4d52d13
Jon Cram <jon@webignition.net>
2020-01-29 13:19:23 +0000

    Use compilable source factory 0.12

--- tests/Fixtures/php/Test/ExampleComFollowMoreInformationTest.php
+++ tests/Fixtures/php/Test/ExampleComFollowMoreInformationTest.php
@@ -2,6 +2,7 @@
 
 namespace webignition\BasilRunner\Generated;
 
+use webignition\BaseBasilTestCase\Statement;
 use webignition\DomElementIdentifier\ElementIdentifier;
 use webignition\BaseBasilTestCase\AbstractBaseTest;
 
@@ -19,18 +20,27 @@ class ExampleComFollowMoreInformationTest extends AbstractBaseTest
         $this->setBasilStepName('follow more information');
 
         // $"a" exists <- click $"a"
+        $statement = Statement::createAssertion('$"a" exists');
+        $this->currentStatement = $statement;
         $has = $this->navigator->hasOne(ElementIdentifier::fromJson('{"locator":"a"}'));
         $this->assertTrue($has);
+        $this->completedStatements[] = $statement;
 
         // click $"a"
+        $statement = Statement::createAction('click $"a"');
+        $this->currentStatement = $statement;
         $element = $this->navigator->findOne(ElementIdentifier::fromJson('{"locator":"a"}'));
         $element->click();
+        $this->completedStatements[] = $statement;
 
         // $page.url is "https://www.iana.org/domains/reserved"
+        $statement = Statement::createAssertion('$page.url is "https://www.iana.org/domains/reserved"');
+        $this->currentStatement = $statement;
         $expected = "https://www.iana.org/domains/reserved" ?? null;
         $expected = (string) $expected;
         $examined = self::$client->getCurrentURL() ?? null;
         $examined = (string) $examined;
         $this->assertEquals($expected, $examined);
+        $this->completedStatements[] = $statement;
     }
 }
