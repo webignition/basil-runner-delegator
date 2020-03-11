2529867f38530ce0cad00190e7581a4b4f173944
Jon Cram <webignition@gmail.com>
2020-03-11 13:21:08 +0000

    Use TerminalStringFormat in TerminalString

--- src/Model/TerminalString.php
+++ src/Model/TerminalString.php
@@ -6,91 +6,19 @@ namespace webignition\BasilRunner\Model;
 
 class TerminalString
 {
-    private const BOLD = "\033[1m";
-
-    private const FG_RED = "\033[31m";
-    private const FG_GREEN = "\033[32m";
-    private const FG_WHITE = "\033[37m";
-
-    private const BG_BLACK = "\033[40m";
-    private const BG_RED = "\033[41m";
-
-    private const RESET = "\033[0m";
-
     public const COLOUR_RED = 'red';
     public const COLOUR_GREEN = 'green';
     public const COLOUR_WHITE = 'white';
     public const COLOUR_BLACK = 'black';
     public const DECORATION_BOLD = 'bold';
 
-    /**
-     * @var array<string, string>
-     */
-    private $foregroundColours = [
-        self::COLOUR_RED => self::FG_RED,
-        self::COLOUR_GREEN => self::FG_GREEN,
-        self::COLOUR_WHITE => self::FG_WHITE,
-    ];
-
-    /**
-     * @var array<string, string>
-     */
-    private $backgroundColours = [
-        self::COLOUR_BLACK => self::BG_BLACK,
-        self::COLOUR_RED => self::BG_RED,
-    ];
-
-    /**
-     * @var array<string, string>
-     */
-    private $decorations = [
-        self::DECORATION_BOLD => self::BOLD,
-    ];
-
-    /**
-     * @var string|null
-     */
-    private $foregroundCode;
-
-    /**
-     * @var string|null
-     */
-    private $backgroundCode;
-
     private $content;
+    private $format;
 
-    /**
-     * @var string[]
-     */
-    private $decorationCodes = [];
-
-    public function __construct(string $content)
+    public function __construct(string $content, ?TerminalStringFormat $format = null)
     {
         $this->content = $content;
-    }
-
-    public function withForegroundColour(string $foregroundColour): TerminalString
-    {
-        $new = clone $this;
-        $new->foregroundCode = $this->foregroundColours[$foregroundColour] ?? null;
-
-        return $new;
-    }
-
-    public function withBackgroundColour(string $backgroundColour): TerminalString
-    {
-        $new = clone $this;
-        $new->backgroundCode = $this->backgroundColours[$backgroundColour] ?? null;
-
-        return $new;
-    }
-
-    public function withDecoration(string $decoration): TerminalString
-    {
-        $new = clone $this;
-        $new->addDecoration($decoration);
-
-        return $new;
+        $this->format = $format instanceof TerminalStringFormat ? $format : new TerminalStringFormat();
     }
 
     public function __toString(): string
@@ -99,29 +27,6 @@ class TerminalString
             return '';
         }
 
-        $foreground = (string) $this->foregroundCode;
-        $background = (string) $this->backgroundCode;
-        $decoration = implode('', $this->decorationCodes);
-
-        $includeReset = '' !== $foreground || '' !== $background || '' !== $decoration;
-
-        return sprintf(
-            '%s%s%s%s%s',
-            $foreground,
-            $background,
-            $decoration,
-            $this->content,
-            $includeReset ? self::RESET : ''
-        );
-    }
-
-    private function addDecoration(string $decoration): void
-    {
-        $decorationCode = $this->decorations[$decoration] ?? null;
-
-        if (is_string($decorationCode)) {
-            $this->decorationCodes[] = $decorationCode;
-            $this->decorationCodes = array_unique($this->decorationCodes);
-        }
+        return sprintf((string) $this->format, $this->content);
     }
 }
--- tests/Unit/Model/TerminalStringTest.php
+++ tests/Unit/Model/TerminalStringTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace webignition\BasilRunner\Tests\Unit\Model;
 
 use webignition\BasilRunner\Model\TerminalString;
+use webignition\BasilRunner\Model\TerminalStringFormat;
 use webignition\BasilRunner\Tests\Unit\AbstractBaseTest;
 
 class TerminalStringTest extends AbstractBaseTest
@@ -24,65 +25,30 @@ class TerminalStringTest extends AbstractBaseTest
                 'terminalString' => new TerminalString(''),
                 'expectedString' => '',
             ],
-            'empty, foreground colour is ignored' => [
-                'terminalString' => (new TerminalString(''))
-                    ->withForegroundColour(TerminalString::COLOUR_RED),
+            'empty, formatting is ignored' => [
+                'terminalString' => new TerminalString(
+                    '',
+                    new TerminalStringFormat([
+                        TerminalStringFormat::FOREGROUND_COLOUR => TerminalStringFormat::COLOUR_RED,
+                    ])
+                ),
                 'expectedString' => '',
             ],
-            'empty, background colour is ignored' => [
-                'terminalString' => (new TerminalString(''))
-                    ->withBackgroundColour(TerminalString::COLOUR_RED),
-                'expectedString' => '',
-            ],
-            'empty, decorations are ignored' => [
-                'terminalString' => (new TerminalString(''))
-                    ->withDecoration(TerminalString::DECORATION_BOLD),
-                'expectedString' => '',
-            ],
-            'non-empty, no colour, no decorations' => [
+            'non-empty, no formatting' => [
                 'terminalString' => new TerminalString('content'),
                 'expectedString' => 'content',
             ],
-            'non-empty, foreground colour red' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withForegroundColour(TerminalString::COLOUR_RED),
-                'expectedString' => "\033[31m" . 'content' . "\033[0m",
-            ],
-            'non-empty, foreground colour green' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withForegroundColour(TerminalString::COLOUR_GREEN),
-                'expectedString' => "\033[32m" . 'content' . "\033[0m",
-            ],
-            'non-empty, foreground colour white' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withForegroundColour(TerminalString::COLOUR_WHITE),
-                'expectedString' => "\033[37m" . 'content' . "\033[0m",
-            ],
-            'non-empty, foreground colour invalid' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withForegroundColour('undefined colour'),
-                'expectedString' => 'content',
-            ],
-            'non-empty, background colour black' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withBackgroundColour(TerminalString::COLOUR_BLACK),
-                'expectedString' => "\033[40m" . 'content' . "\033[0m",
-            ],
-            'non-empty, background colour red' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withBackgroundColour(TerminalString::COLOUR_RED),
-                'expectedString' => "\033[41m" . 'content' . "\033[0m",
-            ],
-            'non-empty, bold decoration' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withDecoration(TerminalString::DECORATION_BOLD),
-                'expectedString' => "\033[1m" . 'content' . "\033[0m",
-            ],
-            'non-empty, foreground colour, background color, bold decoration' => [
-                'terminalString' => (new TerminalString('content'))
-                    ->withForegroundColour(TerminalString::COLOUR_WHITE)
-                    ->withBackgroundColour(TerminalString::COLOUR_RED)
-                    ->withDecoration(TerminalString::DECORATION_BOLD),
+            'non-empty, has format' => [
+                'terminalString' => new TerminalString(
+                    'content',
+                    new TerminalStringFormat([
+                        TerminalStringFormat::FOREGROUND_COLOUR => TerminalString::COLOUR_WHITE,
+                        TerminalStringFormat::BACKGROUND_COLOUR => TerminalString::COLOUR_RED,
+                        TerminalStringFormat::DECORATIONS => [
+                            TerminalStringFormat::DECORATION_BOLD,
+                        ],
+                    ])
+                ),
                 'expectedString' => "\033[37m" . "\033[41m" . "\033[1m" . 'content' . "\033[0m",
             ],
         ];
