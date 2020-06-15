01e34bd9f1ac2cd0f12acf92dd94508778a854cc
Jon Cram <webignition@gmail.com>
2020-06-15 10:39:35 +0100

    Output empty line between steps (#410)

--- tests/Integration/GenerateRunTest.php
+++ tests/Integration/GenerateRunTest.php
@@ -48,16 +48,20 @@ class GenerateRunTest extends TestCase
                     '    ' . $cof->createSuccess('✓') .
                     ' $page.title is "Test fixture web server default document"' . "\n" .
                     '    ' . $cof->createSuccess('✓') . ' $page.title matches "/fixture web server/"' . "\n" .
+                    "\n" .
                     '  ' . $cof->createSuccess('✓') . ' ' . $cof->createSuccess('verify primary heading') . "\n" .
                     '    ' . $cof->createSuccess('✓') . ' $"h1" is "Test fixture web server default document"' . "\n" .
+                    "\n" .
                     '  ' . $cof->createSuccess('✓') . ' ' . $cof->createSuccess('verify links are present') .
                     "\n" .
                     '    ' . $cof->createSuccess('✓') . ' $"a[id=link-to-assertions]" exists' . "\n" .
                     '    ' . $cof->createSuccess('✓') . ' $"a[id=link-to-form]" exists' . "\n" .
+                    "\n" .
                     '  ' . $cof->createSuccess('✓') . ' ' . $cof->createSuccess('navigate to form') . "\n" .
                     '    ' . $cof->createSuccess('✓') . ' click $"a[id=link-to-form]"' . "\n" .
                     '    ' . $cof->createSuccess('✓') . ' $page.url is "http://127.0.0.1:9080/form.html"' . "\n" .
-                    '    ' . $cof->createSuccess('✓') . ' $page.title is "Form"' . "\n"
+                    '    ' . $cof->createSuccess('✓') . ' $page.title is "Form"' . "\n" .
+                    "\n"
             ],
             'failing: single test' => [
                 'source' => './tests/Fixtures/basil-integration/FailingTest/index-page-test.yml',
@@ -68,8 +72,10 @@ class GenerateRunTest extends TestCase
                     '    ' . $cof->createSuccess('✓') . ' $page.url is "http://127.0.0.1:9080/index.html"' . "\n" .
                     '    ' . $cof->createSuccess('✓') .
                     ' $page.title is "Test fixture web server default document"' . "\n" .
+                    "\n" .
                     '  ' . $cof->createSuccess('✓') . ' ' . $cof->createSuccess('verify primary heading') . "\n" .
                     '    ' . $cof->createSuccess('✓') . ' $"h1" is "Test fixture web server default document"' . "\n" .
+                    "\n" .
                     '  ' . $cof->createFailure('x') . ' ' . $cof->createFailure('verify links are present') .
                     "\n" .
                     '    ' . $cof->createFailure('x') . ' ' . $cof->createHighlightedFailure(
@@ -78,7 +84,8 @@ class GenerateRunTest extends TestCase
                     '    * Element ' . $cof->createComment('$"a[id=link-to-assertions]"') . ' identified by:' . "\n" .
                     '        - CSS selector: ' . $cof->createComment('a[id=link-to-assertions]') . "\n" .
                     '        - ordinal position: ' . $cof->createComment('1') . "\n" .
-                    '      does exist' . "\n"
+                    '      does exist' . "\n" .
+                    "\n"
             ],
             'passing: page import with element de-referencing' => [
                 'source' => './tests/Fixtures/basil-integration/Test/form-page-test.yml',
@@ -95,7 +102,8 @@ class GenerateRunTest extends TestCase
                     '    ' . $cof->createSuccess('✓')
                     . ' $"form[action=\'/action1\']" >> $".textarea-non-empty" is "textarea content"' . "\n" .
                     '      ' . $cof->createComment('> resolved from:')
-                    . ' $form_page.elements.textarea_within_form_one is "textarea content"' . "\n"
+                    . ' $form_page.elements.textarea_within_form_one is "textarea content"' . "\n" .
+                    "\n"
             ],
         ];
     }
