193e8031b14b36da661b80693bcf0c24cfd83bde
Jon Cram <webignition@gmail.com>
2020-06-18 15:55:27 +0100

    Remove some ConsoleOutputFactory methods (#431)

--- src/Services/ResultPrinter/ConsoleOutputFactory.php
+++ src/Services/ResultPrinter/ConsoleOutputFactory.php
@@ -9,26 +9,6 @@ use Symfony\Component\Console\Formatter\OutputFormatterStyleInterface;
 
 class ConsoleOutputFactory
 {
-    public function createTestPath(string $path): string
-    {
-        return $this->applyStyle($path, new OutputFormatterStyle(null, null, ['bold']));
-    }
-
-    public function createSuccess(string $text): string
-    {
-        return $this->applyStyle($text, new OutputFormatterStyle('green'));
-    }
-
-    public function createFailure(string $text): string
-    {
-        return $this->applyStyle($text, new OutputFormatterStyle('red'));
-    }
-
-    public function createHighlightedFailure(string $text): string
-    {
-        return $this->applyStyle($text, new OutputFormatterStyle('white', 'red'));
-    }
-
     public function createComment(string $text): string
     {
         return $this->applyStyle($text, new OutputFormatterStyle('yellow'));
