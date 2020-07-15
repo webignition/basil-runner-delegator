cd561fbeda48ab1f56ab239e6d4c65da4029bbdd
Jon Cram <webignition@gmail.com>
2020-07-15 10:53:43 +0100

    Use basil-phpunit-result-printer 0.2 (#496)

--- tests/Integration/GenerateRunTest.php
+++ tests/Integration/GenerateRunTest.php
@@ -7,7 +7,6 @@ namespace webignition\BasilRunner\Tests\Integration;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Yaml\Yaml;
 use webignition\BasilRunner\Tests\Model\PhpUnitOutput;
-use webignition\BasilRunner\Tests\Services\ConsoleStyler;
 
 class GenerateRunTest extends TestCase
 {
@@ -57,75 +56,21 @@ class GenerateRunTest extends TestCase
 
     public function generateAndRunDataProvider(): array
     {
-        $styler = new ConsoleStyler();
-
         return [
             'passing: single test' => [
                 'source' => './tests/Fixtures/basil-integration/Test/index-page-test.yml',
                 'target' => './tests/build/target',
-                'expectedOutputBody' =>
-                    $styler->bold('tests/Fixtures/basil-integration/Test/index-page-test.yml') . "\n" .
-                    '  ' . $styler->success('✓') . ' ' . $styler->success('verify page is open') . "\n" .
-                    '    ' . $styler->success('✓') . ' $page.url is "http://127.0.0.1:9080/index.html"' . "\n" .
-                    '    ' . $styler->success('✓') .
-                    ' $page.title is "Test fixture web server default document"' . "\n" .
-                    '    ' . $styler->success('✓') . ' $page.title matches "/fixture web server/"' . "\n" .
-                    "\n" .
-                    '  ' . $styler->success('✓') . ' ' . $styler->success('verify primary heading') . "\n" .
-                    '    ' . $styler->success('✓') . ' $"h1" is "Test fixture web server default document"' . "\n" .
-                    "\n" .
-                    '  ' . $styler->success('✓') . ' ' . $styler->success('verify links are present') .
-                    "\n" .
-                    '    ' . $styler->success('✓') . ' $"a[id=link-to-assertions]" exists' . "\n" .
-                    '    ' . $styler->success('✓') . ' $"a[id=link-to-form]" exists' . "\n" .
-                    "\n" .
-                    '  ' . $styler->success('✓') . ' ' . $styler->success('navigate to form') . "\n" .
-                    '    ' . $styler->success('✓') . ' click $"a[id=link-to-form]"' . "\n" .
-                    '    ' . $styler->success('✓') . ' $page.url is "http://127.0.0.1:9080/form.html"' . "\n" .
-                    '    ' . $styler->success('✓') . ' $page.title is "Form"' . "\n" .
-                    "\n"
+                'expectedOutputBody' => file_get_contents(__DIR__ . '/../Fixtures/Output/passing-index-page-test.yml'),
             ],
             'failing: single test' => [
                 'source' => './tests/Fixtures/basil-integration/FailingTest/index-page-test.yml',
                 'target' => './tests/build/target',
-                'expectedOutputBody' =>
-                    $styler->bold('tests/Fixtures/basil-integration/FailingTest/index-page-test.yml') . "\n" .
-                    '  ' . $styler->success('✓') . ' ' . $styler->success('verify page is open') . "\n" .
-                    '    ' . $styler->success('✓') . ' $page.url is "http://127.0.0.1:9080/index.html"' . "\n" .
-                    '    ' . $styler->success('✓') .
-                    ' $page.title is "Test fixture web server default document"' . "\n" .
-                    "\n" .
-                    '  ' . $styler->success('✓') . ' ' . $styler->success('verify primary heading') . "\n" .
-                    '    ' . $styler->success('✓') . ' $"h1" is "Test fixture web server default document"' . "\n" .
-                    "\n" .
-                    '  ' . $styler->failure('x') . ' ' . $styler->failure('verify links are present') .
-                    "\n" .
-                    '    ' . $styler->failure('x') . ' ' . $styler->highlightedFailure(
-                        '$"a[id=link-to-assertions]" not-exists'
-                    ) . "\n" .
-                    '    * Element ' . $styler->comment('$"a[id=link-to-assertions]"') . ' identified by:' . "\n" .
-                    '        - CSS selector: ' . $styler->comment('a[id=link-to-assertions]') . "\n" .
-                    '        - ordinal position: ' . $styler->comment('1') . "\n" .
-                    '      does exist' . "\n" .
-                    "\n"
+                'expectedOutputBody' => file_get_contents(__DIR__ . '/../Fixtures/Output/failing-index-page-test.yml'),
             ],
             'passing: page import with element de-referencing' => [
                 'source' => './tests/Fixtures/basil-integration/Test/form-page-test.yml',
                 'target' => './tests/build/target',
-                'expectedOutputBody' =>
-                    $styler->bold('tests/Fixtures/basil-integration/Test/form-page-test.yml') . "\n" .
-                    '  ' . $styler->success('✓') . ' ' . $styler->success('verify page is open') . "\n" .
-                    '    ' . $styler->success('✓') . ' $page.url is "http://127.0.0.1:9080/form.html"' . "\n" .
-                    '    ' . $styler->success('✓') . ' $page.title is "Form"' . "\n" .
-                    '    ' . $styler->success('✓')
-                    . ' $"form[action=\'/action1\']" >> $"input[name=\'input-with-value\']" is "test"' . "\n" .
-                    '      ' . $styler->comment('> resolved from:')
-                    . ' $form_page.elements.input_with_value is "test"' . "\n" .
-                    '    ' . $styler->success('✓')
-                    . ' $"form[action=\'/action1\']" >> $".textarea-non-empty" is "textarea content"' . "\n" .
-                    '      ' . $styler->comment('> resolved from:')
-                    . ' $form_page.elements.textarea_within_form_one is "textarea content"' . "\n" .
-                    "\n"
+                'expectedOutputBody' => file_get_contents(__DIR__ . '/../Fixtures/Output/passing-form-page-test.yml'),
             ],
         ];
     }
