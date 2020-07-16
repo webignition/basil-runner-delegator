fe079502c78316d1f1282dcfa47b3c7337ffd500
Jon Cram <webignition@gmail.com>
2020-07-16 17:09:10 +0100

    Update integration testing scripts

--- docker/integration-build.sh
+++ docker/integration-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cp -R ../tests/Fixtures/html/* nginx/html
-docker-compose -f docker-compose.yml -f docker-compose-test.yml build
-docker-compose -f docker-compose.yml -f docker-compose-test.yml up --detach
+docker-compose -f docker-compose.yml build
+docker-compose -f chrome/docker-compose.yml -f chrome/docker-compose-test.yml build
+docker-compose -f chrome/docker-compose.yml -f chrome/docker-compose-test.yml up --detach
--- docker/integration-test.sh
+++ docker/integration-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose exec basil-runner ./bin/basil-runner generate --source=basil-integration/Test --target=build
-docker-compose exec basil-runner ./bin/basil-runner run --path=build
+docker-compose -f chrome/docker-compose.yml exec basil-runner-chrome ./bin/basil-runner generate --source=basil-integration/Test --target=build
+docker-compose -f chrome/docker-compose.yml exec basil-runner-chrome ./bin/basil-runner run --path=build
