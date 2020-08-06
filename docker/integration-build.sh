882b45651045a6a0fb346c0c6b87e2d6a5baf845
Jon Cram <webignition@gmail.com>
2020-08-06 10:58:26 +0100

    Remove current docker content

--- docker/integration-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cp -R ../tests/Fixtures/html/* nginx/html
-docker-compose -f docker-compose.yml build
-docker-compose -f chrome/docker-compose.yml -f chrome/docker-compose-test.yml build
-docker-compose -f chrome/docker-compose.yml -f chrome/docker-compose-test.yml up --detach
--- docker/integration-test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -f chrome/docker-compose.yml exec basil-runner-chrome ./bin/basil-runner generate --source=basil-integration/Test --target=build
-docker-compose -f chrome/docker-compose.yml exec basil-runner-chrome ./bin/basil-runner run --path=build
