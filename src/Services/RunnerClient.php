3e31cb2334c9fa60bc67b93ae29b39f34750ce8e
Jon Cram <webignition@gmail.com>
2020-08-31 15:32:25 +0100

    Fix runner command

--- src/Services/RunnerClient.php
+++ src/Services/RunnerClient.php
@@ -8,7 +8,7 @@ use webignition\TcpCliProxyClient\Client;
 
 class RunnerClient extends Client
 {
-    private const RUNNER_COMMAND = './bin/runner --target=%s';
+    private const RUNNER_COMMAND = './bin/runner --path=%s';
 
     public function request(string $request, ?callable $filter = null): void
     {
