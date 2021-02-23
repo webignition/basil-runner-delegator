0ad34ac69a1e0a98fe102cb714a8d08973611f30
Jon Cram <webignition@gmail.com>
2021-02-23 11:09:05 +0000

    Add deploy workflow to test

--- deploy/docker_hub_push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_DIRECTORY="$(dirname "$0")"
-source ${CURRENT_DIRECTORY}/../build/.image_data.sh
-source ${CURRENT_DIRECTORY}/docker_hub_login.sh
-
-docker push ${IMAGE_NAME}
